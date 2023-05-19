ebca3c76af6c0fab215a6b45e5bdaa4de97e34e9
john-mirage <28307090+john-mirage@users.noreply.github.com>
2023-05-19 17:23:18 +0200

    fix: fix paths

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        restaurant1: resolve(__dirname, 'src/pages/a-la-francaise.html'),
-        restaurant2: resolve(__dirname, 'src/pages/la-note-enchantee.html'),
-        restaurant3: resolve(__dirname, 'src/pages/la-palette-du-gout.html'),
-        restaurant4: resolve(__dirname, 'src/pages/le-delice-des-sens.html'),
-      }
-    }
-  }
-})
+        main: resolve(__dirname, "index.html"),
+        restaurant1: resolve(__dirname, "a-la-francaise.html"),
+        restaurant2: resolve(__dirname, "la-note-enchantee.html"),
+        restaurant3: resolve(__dirname, "la-palette-du-gout.html"),
+        restaurant4: resolve(__dirname, "le-delice-des-sens.html"),
+      },
+    },
+  },
+});
